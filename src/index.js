a5f3bdf7d4e4f5eaaed5deff20b54af6cad51c40
Dmytro Iovenko <iovenko@gmail.com>
2024-08-23 13:01:21 -0400

    get dbX from central database

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
-function getUserData(id) {
+async function getUserData(id) {
   const dbs = {
     db1: db1,
     db2: db2,
-    db3: db3
+    db3: db3,
   };
+  // Get apropriate db1, db2, or db3 from central database
+  const dbX = await central(id);
+  console.log(dbX);
 }
+
+getUserData(10);
