450a721229eb463831552edaabc0ffa7d7f9c80d
Dmytro Iovenko <iovenko@gmail.com>
2024-08-23 14:02:14 -0400

    add testing for invalid data types

--- src/index.js
+++ src/index.js
@@ -11,19 +11,24 @@ async function getUserData(id) {
   };
   // function must complete in 200ms or less
   const start = Date.now();
-  // Get apropriate db1, db2, or db3 from central database
-  const dbX = await central(id);
-  // Use dbX value to get user data and vault data simultaneously
-  const [userData, vaultData] = await Promise.all([dbs[dbX](id), vault(id)]);
-  // if it is DEBUG mode, log elapsed time
-  DEBUG && console.log("Time elsapsed:", Date.now() - start);
-  // combine all specific data associated with the user with the given id
-  // into a single object
-  const combinedUserData = { id, ...userData, ...vaultData };
-  // if it is DEBUG mode, log combined user data
-  //   DEBUG && console.log("User data:", combinedUserData);
-  // return a Promise that resolves to the object with combined user data
-  return combinedUserData;
+  try {
+    // Get apropriate db1, db2, or db3 from central database
+    const dbX = await central(id);
+    // Use dbX value to get user data and vault data simultaneously
+    const [userData, vaultData] = await Promise.all([dbs[dbX](id), vault(id)]);
+    // combine all specific data associated with the user with the given id
+    // into a single object
+    const combinedUserData = { id, ...userData, ...vaultData };
+    // if it is DEBUG mode, log combined user data
+    DEBUG && console.log("User data:", combinedUserData);
+    // return a Promise that resolves to the object with combined user data
+    return combinedUserData;
+//   } catch (error) {
+//     console.log(error);
+  } finally {
+    // if it is DEBUG mode, log elapsed time
+    DEBUG && console.log("Time elsapsed:", Date.now() - start);
+  }
 }
 
 // Test code by passing Valid numbers – 1 through 10 (inclusive)
@@ -40,6 +45,15 @@ async function getUserData(id) {
   getUserData(id)
     // log combined user data
     .then((userData) => console.log("ID:", id, "USER DATA:", userData))
+    // log error, if any
+    .catch((error) => console.log("ID:", id, "ERROR:", error.message))
+);
+
+// Invalid data types – strings, Booleans, etc.
+["1", "string", true, false].forEach((id) =>
+  getUserData(id)
+    // log combined user data
+    .then((userData) => console.assert("ID:", id, "USER DATA:", userData))
     // log error, if any
     .catch((error) => console.log("ID:", id, "ERROR:", error.message))
 );
